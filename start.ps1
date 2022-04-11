483d59e28262a7f4e2a18aa2fc9e9fc5ac9c54b6
rickjacobo <71619054+rickjacobo@users.noreply.github.com>
2022-04-11 07:11:03 -0700

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -2,8 +2,14 @@ rm -rf /configurations/*
 
 $ReadHost = Read-Host "
 1. Roaming Point to Point VPN (Mobile Wireguard Client to Server Configuration)
+2. Gateway Configuration
 
 Please enter a selection"
 if ($ReadHost -eq "1") {
     pwsh roaming-point-to-point.ps1
 }
+
+if ($ReadHost -eq "1") {
+    pwsh gateway.ps1
+}
+
